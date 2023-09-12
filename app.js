d7c6e3f4f4ece850c77fcd0ea2f5dc12ba935b05
Davi Cabral <davicabral01@gmail.com>
2023-09-12 09:21:11 -0300

    teste

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const imoveis = require('./src/routes/imoveis')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './src/views')
+
+app.use(imoveis)
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(3000, () => {
+  console.log('Aplicação executando na porta 3000')
+})
