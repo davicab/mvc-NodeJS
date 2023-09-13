8dacba653cbeab4366593f464daf05bf1359f805
davicab <davicabral01@gmail.com>
2023-09-13 01:03:45 -0300

    alteracoes

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -11,6 +11,25 @@ function index(req, res) {
 }
 function adicionarTarefa(req, res) {
   const { name } = req.body;
+  const tarefas = getTarefas()
+  try {
+    const data = tarefas; 
+    const novaTarefa = {
+      id: data.length + 1,
+      name,
+      concluida: false 
+    };
+    tarefas
+
+    // Escreve os dados atualizados de volta no arquivo JSON
+    fs.writeFileSync('../caminho-para-o-arquivo/tarefas.json', JSON.stringify(data, null, 2));
+
+    return true; // Indica que a tarefa foi adicionada com sucesso
+  } catch (error) {
+    console.error('Erro ao adicionar tarefa:', error);
+    return false; // Indica que ocorreu um erro ao adicionar a tarefa
+  }
+
   console.log(name)
-  res.render('tarefas/add', { req: name })
+  // res.render('tarefas/add', { req: name })
 }
